57e4714aa719a1a8288afdbe2c96e008067607eb
Dasha <dasha@example.com>
2026-09-04 09:27:14 +0300

    Pr2

--- lib/state/pet_list_notifier.dart
+++ lib/state/pet_list_notifier.dart
@@ -31,9 +31,9 @@ class PetListNotifier extends ChangeNotifier {
     notifyListeners();
 
     try {
+      //throw Exception('Сервер недоступен');
       _result = await _repository.find(_query);
       _status = LoadStatus.success;
-      throw Exception('Сервер недоступен');
     } catch (e) {
       _error = 'Не удалось загрузить список: $e';
       _status = LoadStatus.error;
